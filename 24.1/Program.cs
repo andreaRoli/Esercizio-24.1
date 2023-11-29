bb196d252077c9052d00cfc17ede769ec2c6200f
andreaRoli <aggiustatuttobob4@gmail.com>
2023-11-29 15:10:07 +0100

    Aggiungere i file di progetto.

---
+++ 24.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato c=new Quadrato();
+
+            Console.WriteLine("Inserisci la misura del lato");
+            c.Lato=float.Parse(Console.ReadLine());
+
+            float superficie = c.Lato * c.Lato;
+
+            Console.WriteLine("L'are del quadrato è: {0}", superficie);
+            Console.ReadLine();
+        }
+    }
+}
